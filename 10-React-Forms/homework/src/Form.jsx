20c237d838768457f64dcdd46a2e557d5ac07a28
mijail pulgar <mijail86@gmail.com>
2022-08-08 18:51:18 -0500

    form lista con extra en el submit

--- 10-React-Forms/homework/src/Form.jsx
+++ 10-React-Forms/homework/src/Form.jsx
@@ -1,9 +1,93 @@
 import React from 'react';
 
+
+
+export function validate(input) {
+  let errors = {};
+  if (!input.username) {
+    errors.username = 'Username is required';
+  } else if (!/\S+@\S+\.\S+/.test(input.username)) {
+    errors.username = 'Username is invalid';
+  }
+  else if(!input.password){
+    errors.password = 'Password is required';
+  }else if(!/(?=.*[0-9])/.test(input.password)){
+    errors.password = 'Password is invalid';
+  }
+
+  return errors;
+};
+
 export default function  Form() {
+
+  const [errors, setErrors] = React.useState({
+    username: '',
+    password: '',
+  });
+
+  const [input, setInput] = React.useState({
+    username: '',
+    password: '',
+  });
+  
+
+  const handleInputChange = function(e) {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value
+    });
+    setErrors(validate({
+      ...input,
+      [e.target.name]: e.target.value
+    }));
+  }
+
+  //para validar que no estamos enviando el form con errores o vacio
+  function validateSubmit(e) {
+    e.preventDefault(); // evita que se envie por defecto
+    if (errors.username || errors.password) {
+      console.log('Hay campos por completar');
+    }else{
+      console.log('Form enviado');
+    }
+  }
+
   return (
       <div>
-        Componente Form
+        <form onSubmit={(e) => validateSubmit(e)}>
+          <div>
+            <label>Username:</label>
+            <input 
+              className={errors.username && 'danger'}
+              type="text"
+              name="username" 
+              value={input.username} 
+              placeholder="username" 
+              onChange={handleInputChange}
+            />
+            {errors.username && (
+              <p className="danger">{errors.username}</p>
+            )}
+            
+          </div>
+
+          <div>
+            <label>Password:</label>
+            <input 
+              className={errors.password && 'danger'}
+              name="password" 
+              value={input.password} 
+              placeholder="password" 
+              type="password" 
+              onChange={handleInputChange}
+            />
+            {errors.password && (
+              <p className="danger">{errors.password}</p>
+            )}
+          </div>
+          
+          <input type="submit" value="Submit"/>
+        </form>
       </div>
   )
 }
